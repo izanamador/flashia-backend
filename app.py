12468d8854a94e38b8992cbbcb6d185745a8924f
Izan Amador <izan.amador@gmail.com>
2024-10-05 23:20:16 +0200

    test

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/tmp'  # Change to use /tmp directory
+app.config['UPLOAD_FOLDER'] = '/tmp'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max-limit
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def index():
+    return "Flashia is running!"
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -25,19 +29,14 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-# Add this new route to handle POST requests explicitly
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return 'No file part'
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file'
-    if file:
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        return render_template('index.html', filename=filename)
+@app.route('/debug')
+def debug():
+    return {
+        "env": dict(os.environ),
+        "pwd": os.getcwd(),
+        "files": os.listdir(),
+        "templates": os.listdir("templates") if os.path.exists("templates") else "No templates directory"
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
