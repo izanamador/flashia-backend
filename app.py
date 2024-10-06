b5f2c107a9ef5d4c73d100119e6139103fab6d79
Izan Amador <izan.amador@gmail.com>
2024-10-06 20:33:45 +0200

    test

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+import os
 import PIL
 import requests
 import torch
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
 
 app = FastAPI()
 
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="templates")
+
+# Set up static file serving
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Load the model
 model_id = "timbrooks/instruct-pix2pix"
@@ -27,7 +32,7 @@ def download_image(url):
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "image_url": None})
 
 @app.get("/generate")
 async def generate():
@@ -38,10 +43,14 @@ async def generate():
     images = pipe(prompt, image=image, num_inference_steps=15, image_guidance_scale=1).images
     
     # Save the generated image
-    output_path = "output.png"
+    output_path = "static/output.png"  # Save to the static folder
     images[0].save(output_path)
     
     return {"image_url": output_path}
+
+@app.get("/show-image")
+async def show_image():
+    return templates.TemplateResponse("index.html", {"request": Request, "image_url": "output.png"})
 
 if __name__ == "__main__":
     import uvicorn
